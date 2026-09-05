2ab537a395653d106ae2f98332e9b53add1e6069
246Sudankhanal <sudankhanal246@gmail.com>
2026-09-05 13:24:03 +1000

    FIxing nextconfig error

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,11 @@ import { APP_DOMAIN } from "@/lib/brand";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  allowedDevOrigins: ["quickly-tactics-uninstall.ngrok-free.dev"],
-  serverActions: {
-    allowedOrigins: [APP_DOMAIN, `www.${APP_DOMAIN}`],
-  },
+  allowedDevOrigins: [
+    "quickly-tactics-uninstall.ngrok-free.dev",
+    APP_DOMAIN,
+    `www.${APP_DOMAIN}`,
+  ],
 };
 
 export default nextConfig;
